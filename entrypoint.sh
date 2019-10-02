8b1a0e2c937c93485defdb6cec7dfc32e4b205f4
joslinmicrosoft <54335291+joslinmicrosoft@users.noreply.github.com>
2019-10-02 11:16:43 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -l
+echo "Auzure Pages Build Host: $INPUT_AZURE_PAGES_BUILDHOST"
+UPLOAD_HOST="https://$INPUT_AZURE_PAGES_BUILDHOST/api/closepr?apiToken=$INPUT_AZURE_PAGES_API_TOKEN&apiVersion=$INPUT_AZURE_PAGES_API_VERSION"
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+curl -v -F "event=@$GITHUB_EVENT_PATH" $UPLOAD_HOST
+
+if [ 0 -eq $? ]; then
+        echo "\e[32mSuccessfully uploaded webhook information\e[0m"
+else
+        echo "\e[31mFailed to upload webhook information\e[0m"
+    exit 1
+fi;
